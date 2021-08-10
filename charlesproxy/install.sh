42f962d0aec25ce770a29cf7a6b087f0bb98aafc
Nathan Read <nread@bink.com>
2021-08-10 16:51:51 +0100

    adding pkill to script

--- charlesproxy/install.sh
+++ charlesproxy/install.sh
@@ -4,6 +4,7 @@ install_charlesproxy() {
     apiversion="$(curl -sS https://www.charlesproxy.com/latest-release/download.do\# | grep -E '(v[0-9]\.[0-9]\.[0-9])' |  cut -d '>' -f2 | cut -d '<' -f1 | cut -d 'v' -f2)"
     curl https://www.charlesproxy.com/assets/release/$apiversion/charles-proxy-$apiversion.dmg -o /tmp/charlesproxy.dmg
     yes Y | hdiutil attach /tmp/charlesproxy.dmg >/dev/null
+    pkill Charles
     if [ -d "/Applications/Charles.app" ]; then
         rm -rf "/Applications/Charles.app"
     fi
