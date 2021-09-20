914377c9786a9ecac5e2f601bff0f7e03e69b1f4
Nathan Read <nread@bink.com>
2021-09-20 17:46:34 +0100

    insomnia install script

---
+++ insomia.sh
@@ -0,0 +1,32 @@
+#!/bin/zsh
+
+set -e
+
+uninstall() {
+    if [ -d "/Applications/Insomnia.app" ]
+    then
+        echo "$(date -u) - Uninstalling Insomnia"
+        killall -KILL "Insomnia" 2>/dev/null | true
+        rm -rf /Applications/Insomnia.app
+    else
+        echo "$(date -u) - Insomnia already uninstalled, skipping uninstall"
+    fi
+}
+
+install() {
+    uninstall
+    echo "$(date -u) - Installing Insomnia"
+    curl -sS -L "https://updates.insomnia.rest/downloads/mac/latest?app=com.insomnia.app" -o "/tmp/insomnia.dmg"
+    hdiutil attach "/tmp/insomnia.dmg" >/dev/null
+    cp -R "/Volumes/""$(ls /Volumes/ | grep "^Insomnia")""/Insomnia.app" "/Applications"
+    hdiutil eject "/Volumes/""$(ls /Volumes/ | grep "^Insomnia")" >/dev/null
+    rm "/tmp/insomnia.dmg"
+}
+
+if [[ $4 == "install" ]]; then
+    install
+elif [[ $4 == "uninstall" ]]; then
+    uninstall
+else
+    echo "Unrecognised command, exiting..."
+fi
